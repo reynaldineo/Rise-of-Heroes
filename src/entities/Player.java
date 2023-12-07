897ab465022f3bde43209c40ebccf3deda97525e
vannndar <thariq.ivan@gmail.com>
2023-12-07 16:06:02 +0700

    Feat: Gravity (Bug)

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -5,10 +5,8 @@ import static utils.Constans.Directions.LEFT;
 import static utils.Constans.Directions.RIGHT;
 import static utils.Constans.Directions.UP;
 import static utils.Constans.PlayerConstants.GetSpriteAmount;
-import static utils.Constans.PlayerConstants.IDLE;
-import static utils.Constans.PlayerConstants.RUNNING;
-import static utils.Constans.PlayerConstants.ATTACK;
-import static utils.HelpMethods.CanMoveHere;
+import static utils.Constans.PlayerConstants.*;
+import static utils.HelpMethods.*;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -25,11 +23,18 @@ public class Player extends Entity {
 	private int aniTick, aniIndex, aniSpeed = 30;
 	private int playerAction = IDLE;
 	private boolean moving = false, attacking = false;
-	private boolean left, up, right, down;
+	private boolean left, up, right, down, jump;
 	private float playerSpeed = 3.0f;
 	private int[][] lvlData;
 	private float xDrawOffset = 39 * Game.SCALE;
 	private float yDrawOffset = 50 * Game.SCALE;
+	private boolean inAir = false;
+
+	// Jumping / Gravity
+	private float airSpeed = 0.0f;
+	private float gravity = 0.04f * Game.SCALE;
+	private float jumpSpeed = -2.25f * Game.SCALE;
+	private float fallSpeedAfterCollision = 0.5f * Game.SCALE;
 
 	public Player(float x, float y, int width, int height) {
 		super(x, y, width, height);
@@ -44,9 +49,9 @@ public class Player extends Entity {
 	}
 
 	public void render(Graphics g) {
-		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x-xDrawOffset), (int) (hitbox.y-yDrawOffset),
+		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset),
 				width, height, null);
-		drawHitbox(g);
+		 drawHitbox(g);
 	}
 
 	private void updateAnimationTick() {
@@ -73,6 +78,13 @@ public class Player extends Entity {
 			aniSpeed = 20;
 		}
 
+		if (inAir) {
+			if (airSpeed < 0)
+				playerAction = JUMP;
+			else
+				playerAction = FALLING;
+		}
+
 		if (attacking) {
 			playerAction = ATTACK;
 			aniSpeed = 15;
@@ -92,26 +104,62 @@ public class Player extends Entity {
 
 	private void updatePos() {
 		moving = false;
-		if (!left && !right && !up && !down)
+
+		if (jump)
+			jump();
+		
+		if (!left && !right && !inAir)
 			return;
 
-		float xSpeed = 0, ySpeed = 0;
+		float xSpeed = 0;
 
-		if (left && !right)
-			xSpeed = -playerSpeed;
-		else if (!left && right)
-			xSpeed = playerSpeed;
+		if (left)
+			xSpeed -= playerSpeed;
+		if (right)
+			xSpeed += playerSpeed;
 
-		if (up && !down)
-			ySpeed = -playerSpeed;
-		else if (!up && down)
-			ySpeed = playerSpeed;
+		if (!inAir)
+			if (!IsEntityOnFloor(hitbox, lvlData))
+				inAir = true;
 
-		if (!CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+		if (inAir) {
+			if (!CanMoveHere(hitbox.x, hitbox.y + airSpeed, hitbox.width, hitbox.height, lvlData)) {
+				hitbox.y += airSpeed;
+				airSpeed += gravity;
+				updateXpos(xSpeed);
+			} else {
+				hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, airSpeed);
+
+				if (airSpeed > 0)
+					resetInAir();
+				else
+					airSpeed = fallSpeedAfterCollision;
+				updateXpos(xSpeed);
+			}
+		} else
+			updateXpos(xSpeed);
+		moving = true;
+
+	}
+
+	private void jump() {
+		if (inAir)
+			return;
+		inAir = true;
+		airSpeed = jumpSpeed;
+
+	}
+
+	private void resetInAir() {
+		inAir = false;
+		airSpeed = 0;
+	}
+
+	private void updateXpos(float xSpeed) {
+		if (!CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData))
 			hitbox.x += xSpeed;
-			hitbox.y += ySpeed;
-			moving = true;
-		}
+		else
+			hitbox.x = GetEntityXPosNextToWall(hitbox, xSpeed);
 
 	}
 
@@ -127,6 +175,8 @@ public class Player extends Entity {
 
 	public void loadLevelData(int[][] lvlData) {
 		this.lvlData = lvlData;
+		if (!IsEntityOnFloor(hitbox, lvlData))
+			inAir = true;
 	}
 
 	public boolean isLeft() {
@@ -170,6 +220,10 @@ public class Player extends Entity {
 
 	public void setAttacking(Boolean attacking) {
 		this.attacking = attacking;
+	}
+
+	public void setJump(Boolean jump) {
+		this.jump = jump;
 	}
 
 }
--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -25,18 +25,21 @@ public class KeyboardInputs implements KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		switch (e.getKeyCode()) {
-		case KeyEvent.VK_W:
-			gamePanel.getGame().getPlayer().setUp(false);
-			break;
-		case KeyEvent.VK_A:
-			gamePanel.getGame().getPlayer().setLeft(false);
-			break;
-		case KeyEvent.VK_S:
-			gamePanel.getGame().getPlayer().setDown(false);
-			break;
-		case KeyEvent.VK_D:
-			gamePanel.getGame().getPlayer().setRight(false);
-			break;
+			case KeyEvent.VK_W:
+				gamePanel.getGame().getPlayer().setUp(false);
+				break;
+			case KeyEvent.VK_A:
+				gamePanel.getGame().getPlayer().setLeft(false);
+				break;
+			case KeyEvent.VK_S:
+				gamePanel.getGame().getPlayer().setDown(false);
+				break;
+			case KeyEvent.VK_D:
+				gamePanel.getGame().getPlayer().setRight(false);
+				break;
+			case KeyEvent.VK_SPACE:
+				gamePanel.getGame().getPlayer().setJump(false);
+				break;
 		}
 
 	}
@@ -45,18 +48,21 @@ public class KeyboardInputs implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 
 		switch (e.getKeyCode()) {
-		case KeyEvent.VK_W:
-			gamePanel.getGame().getPlayer().setUp(true);
-			break;
-		case KeyEvent.VK_A:
-			gamePanel.getGame().getPlayer().setLeft(true);
-			break;
-		case KeyEvent.VK_S:
-			gamePanel.getGame().getPlayer().setDown(true);
-			break;
-		case KeyEvent.VK_D:
-			gamePanel.getGame().getPlayer().setRight(true);
-			break;
+			case KeyEvent.VK_W:
+				gamePanel.getGame().getPlayer().setUp(true);
+				break;
+			case KeyEvent.VK_A:
+				gamePanel.getGame().getPlayer().setLeft(true);
+				break;
+			case KeyEvent.VK_S:
+				gamePanel.getGame().getPlayer().setDown(true);
+				break;
+			case KeyEvent.VK_D:
+				gamePanel.getGame().getPlayer().setRight(true);
+				break;
+			case KeyEvent.VK_SPACE:
+				gamePanel.getGame().getPlayer().setJump(true);
+				break;
 		}
 
 	}
